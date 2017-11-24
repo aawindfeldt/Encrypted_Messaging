a1de46496cafe2f2094a1742f65d404dc13342d3
Drew Windfeldt <aawindfeldt@gmail.com>
2017-11-24 10:03:58 -0600

    Create MyChatApp.java

---
+++ Lan Messenger/Java/MyChatApp package/MyChatApp.java
@@ -0,0 +1,11 @@
+package mychatapp;
+
+import mychatapp.gui.MainScreen;
+public class MyChatApp {
+
+    public static void main(String[] args) {
+        MainScreen screen = new MainScreen();
+        screen.show();
+    }
+    
+}
