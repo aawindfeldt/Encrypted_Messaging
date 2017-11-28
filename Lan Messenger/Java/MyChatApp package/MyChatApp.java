acf85b279aeb1e124404894a149f530b1513ffb8
Drew Windfeldt <aawindfeldt@gmail.com>
2017-11-28 13:06:58 -0600

    Update MyChatApp.java

--- Lan Messenger/Java/MyChatApp package/MyChatApp.java
+++ Lan Messenger/Java/MyChatApp package/MyChatApp.java
@@ -1,8 +1,19 @@
-package mychatapp;
-
 import mychatapp.gui.MainScreen;
+
+/**
+ * <h1>MyChatApp</h1>
+ * <p>
+ * This class generates the GUI that is used to run operations.
+ * @author Andrew Windfeldt
+ * @version 1.0
+ * @since 2017-11-24
+ */
 public class MyChatApp {
 
+    /**
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         MainScreen screen = new MainScreen();
         screen.show();
