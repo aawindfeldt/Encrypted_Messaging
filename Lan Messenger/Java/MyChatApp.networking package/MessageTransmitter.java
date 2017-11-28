d6c2c801bb356d44ff1b511cdee6f83c174caf01
Drew Windfeldt <aawindfeldt@gmail.com>
2017-11-28 13:11:39 -0600

    Update MessageTransmitter.java

--- Lan Messenger/Java/MyChatApp.networking package/MessageTransmitter.java
+++ Lan Messenger/Java/MyChatApp.networking package/MessageTransmitter.java
@@ -5,6 +5,14 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * <h1>MessageTransmitter</h1>
+ * <p>
+ * Creates an object to send a message to a specified port
+ * @author Andrew Windfeldt
+ * @version 1.0
+ * @since 2017-11-24
+ */
 public class MessageTransmitter extends Thread{
     String message, hostname;
     int port;
@@ -12,6 +20,13 @@ public class MessageTransmitter extends Thread{
     public MessageTransmitter(){
         
     }
+    /**
+     * Constructor of class
+     * @param message line of text to be sent
+     * @param hostname name of IP address to send the message
+     * @param port number of the port to be sent to
+     * @param gui 
+     */
     public MessageTransmitter(String message, String hostname, int port, WriteableGUI gui){
         this.message = message;
         this.hostname = hostname;
@@ -25,9 +40,10 @@ public class MessageTransmitter extends Thread{
             Socket s = new Socket(hostname, port);
             s.getOutputStream().write(message.getBytes());
             s.close();
-            gui.write("->"+message);
+            gui.write("You: "+message);
         } catch (IOException ex) {
             Logger.getLogger(MessageTransmitter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
+
