7c644d432eb2de7cf7ce7e3821583e7b784b967b
Drew Windfeldt <aawindfeldt@gmail.com>
2017-11-28 13:12:31 -0600

    Update WriteableGUI.java

--- Lan Messenger/Java/MyChatApp.networking package/WriteableGUI.java
+++ Lan Messenger/Java/MyChatApp.networking package/WriteableGUI.java
@@ -1,6 +1,12 @@
 //make this filetype java interface, not java class
 package mychatapp.networking;
 
+/**
+ *
+ * @author Andrew Windfeldt
+ * @version 1.0
+ * @since 2017-11-24
+ */
 public interface WriteableGUI {
     void write(String s);
 }
