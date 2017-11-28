eca9994a35e0c5567c505b92b2fc402786782b8b
Drew Windfeldt <aawindfeldt@gmail.com>
2017-11-28 13:10:31 -0600

    Update MessageListener.java

--- Lan Messenger/Java/MyChatApp.networking package/MessageListener.java
+++ Lan Messenger/Java/MyChatApp.networking package/MessageListener.java
@@ -8,10 +8,24 @@ import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *<h1>MessageListener</h1>
+ * <p>
+ * Class creates an object to listen for an 
+ * incoming message from specified port
+ * @author Andrew Windfeldt
+ * @version 1.0
+ * @since 2017-11-24
+ */
 public class MessageListener extends Thread{
     ServerSocket server;
     int port = 8877;
     WriteableGUI gui;
+    /**
+     * Constructor of the MessageLstener class that listens to specified port
+     * @param gui GUI to print to 
+     * @param port number ID of port being created
+     */
     public MessageListener(WriteableGUI gui, int port){
         this.port = port;
         this.gui = gui;
@@ -21,6 +35,9 @@ public class MessageListener extends Thread{
             Logger.getLogger(MessageListener.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    /**
+     * Overloaded constructor that uses default value for port
+     */
     public MessageListener(){
         try {
             server = new ServerSocket(port);
@@ -37,7 +54,7 @@ public class MessageListener extends Thread{
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 String line = br.readLine();
                 if(line != null){
-                    gui.write("<-"+line);
+                    gui.write("Friend: "+line);
                 }
             }
                 } catch (IOException ex) {
@@ -45,3 +62,4 @@ public class MessageListener extends Thread{
         }
     }
 }
+
